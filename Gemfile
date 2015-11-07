3908b86e08200a3735edd97174ddf4e285c54543
Michael Avila <me@michaelavila.com>
2015-11-07 01:54:26 -0800

    Sign and verify text and file of message
    When the file and text are first received they are signed using the
    burnbox gpg key. Later, when viewing/downloading the message it is
    first verified. If verification fails, then an error is thrown.
    
    You can create a gpg key using `gpg --gen-key` that has the email
    `burnbox@thingssimple.com` and NO passcode.
    
    Let me know what you think.

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 1.2'
 # Paperclip for file uploads
 gem 'paperclip', '~> 4.1'
 
+gem 'gpgme'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 require 'base64'
+require 'gpgme'
 
 class MessagesController < ApplicationController
   def new
@@ -7,15 +8,17 @@ class MessagesController < ApplicationController
 
   def create
     file = message_params[:file]
+    signed_text = Base64.encode64(sign(message_params[:text]).to_s)
     @message = if file
+                 signed_file = Base64.encode64(sign(file.read).to_s)
                  Message.new(
-                   text: message_params[:text],
-                   file_content: Base64.encode64(file.read),
+                   text: signed_text,
+                   file_content: signed_file,
                    file_type: file.content_type,
                    file_extension: file.original_filename.split('.').last,
                  )
                else
-                 Message.new(text: message_params[:text])
+                 Message.new(text: signed_text)
                end
     if @message.save
       render :create
@@ -26,6 +29,7 @@ class MessagesController < ApplicationController
 
   def show
     @message = Message.find_by! slug: params[:slug]
+    @verified_text = verify Base64.decode64(@message.text)
     if not @message.file_content?
       @message.destroy
     end
@@ -33,8 +37,19 @@ class MessagesController < ApplicationController
 
   def download
     @message = Message.find_by! slug: params[:slug]
-    send_data(Base64.decode64(@message.file_content), type: @message.file_type, filename: "#{@message.slug}.#{@message.file_extension}") 
+    verified_file = verify Base64.decode64(@message.file_content)
+    send_data(verified_file, type: @message.file_type, filename: "#{@message.slug}.#{@message.file_extension}")
     @message.destroy
+  end
+
+  def sign data
+    GPGME::Crypto.new.sign data, :signer => "burnbox@thingssimple.com"
+  end
+
+  def verify data
+    GPGME::Crypto.new.verify data do |sig|
+      raise "Failed Verification" if not sig.valid?
+    end
   end
 
   def message_params
