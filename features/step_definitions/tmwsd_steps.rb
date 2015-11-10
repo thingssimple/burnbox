980a8d3809486eea748703878d7a617605a1e96d
Brian Riley <brian@btriley.com>
2015-11-09 23:14:52 -0800

    Add jbuilder responses for the create and new views

--- features/step_definitions/tmwsd_steps.rb
+++ features/step_definitions/tmwsd_steps.rb
@@ -1,50 +1,64 @@
-Given /^I am on the homepage$/ do
+Given(/^I am on the homepage$/) do
   visit "/"
 end
 
-Given /^a message$/ do
+Given(/^a message$/) do
   @message = Message.create text: "This is a test message"
   @message_text = @message.text
 end
 
-Given /^a message with a file$/ do
+Given(/^a message with a file$/) do
   @message = Message.create! file_contents: "some test", file_extension: "txt"
 end
 
-When /^I create a message$/ do
+When(/^I create a message$/) do
   @message_text = "This is a message created through the web page"
   fill_in "Type a message here", with: @message_text
   click_button "Upload"
 end
 
-When /^I go to that message's page$/ do
+When(/^I go to that message's page$/) do
   visit message_path(@message)
 end
 
-Then /^I should see the message$/ do
+Then(/^I should see the message$/) do
   expect(page).to have_content @message_text
 end
 
-Then /^I should see the file$/ do
+Then(/^I should see the file$/) do
   url = download_url(@message)
 
   expect(page.body).to include(url)
   visit url
 end
 
-Then /^the message should deleted$/ do
+Then(/^the message should deleted$/) do
   expect(Message.where(id: @message.id)).to be_empty
 end
 
-Then /^I should see the message URL$/ do
+Then(/^I should see the message URL$/) do
   expect(page).to have_field("link", with: message_url(Message.first))
 end
 
-When /^I create a message with a file$/ do
+When(/^I create a message with a file$/) do
   attach_file "message[file]", Rails.root.join("features", "fixtures", "upload.txt")
   click_button "Upload"
 end
 
-Then /^the message should have a file$/ do
+Then(/^the message should have a file$/) do
   expect(Message.first.file_contents).to_not be_nil
+end
+
+When(/^I POST the following messages:$/) do |table|
+  header "Accept", "application/json"
+  header "Content-Type", "application/json"
+  post messages_path, {message: table.rows_hash}.to_json
+end
+
+Then(/^I should see "(.*?)"$/) do |content|
+  expect(last_response.body).to eq content
+end
+
+Then(/^the response should include a "(.*?)"$/) do |key|
+  expect(JSON.parse(last_response.body)).to have_key(key)
 end
