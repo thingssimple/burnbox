83f0a2de0b8568936bd1d10d6ff3f9016494496a
Brian Riley <brian@btriley.com>
2015-11-09 18:12:19 -0800

    Add extention in the case that it is not already enabled (though I enabled it on production already)

--- db/migrate/20151109031907_change_ids_to_uui_ds.rb
+++ db/migrate/20151109031907_change_ids_to_uui_ds.rb
@@ -1,5 +1,6 @@
 class ChangeIdsToUuiDs < ActiveRecord::Migration
   def change
+    enable_extension "uuid-ossp"
     change_table :messages do |t|
       t.remove :id
       t.uuid :id, primary_key: true, default: "uuid_generate_v4()"
