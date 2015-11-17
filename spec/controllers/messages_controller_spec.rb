16fd1fbbbb4ce483a95a00e57aa33b6c3da1fa4c
Michael Avila <me@michaelavila.com>
2015-11-17 12:06:39 -0800

    Change unit tests and code to go green

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -30,52 +30,61 @@ describe MessagesController do
   end
 
   describe "showing messages" do
-    let(:message) { Message.new(text: "secret message") }
+    let(:key)            { "test key" }
+    let(:secret_message) { "secret message" }
+    let(:guid)           { "1b0dad15-732e-45cc-8da4-749b4b21585d" }
+    let(:message)        { Message.new(text: secret_message) }
+    let(:crypt)          { Crypt.new message }
 
     before do
-      allow(Message).to receive(:find).with("1b0dad15-732e-45cc-8da4-749b4b21585d") { message }
+      allow(Crypt).to receive(:find).with(guid, key) { crypt }
+      allow(crypt).to receive(:text) { "secret message" }
     end
 
     it "shows an existing message" do
-      get :show, id: "1b0dad15-732e-45cc-8da4-749b4b21585d"
+      get :show, id: guid, key: key
 
-      expect(assigns :message).to eq message
+      expect(assigns :message).to eq crypt
     end
 
     it "deletes a message after it's been viewed" do
       allow(message).to receive(:file_contents) { nil }
-      expect(message).to receive(:destroy)
+      expect(crypt).to receive(:destroy)
 
-      get :show, id: "1b0dad15-732e-45cc-8da4-749b4b21585d"
+      get :show, id: guid, key: key
     end
 
     it "doesn't delete messages with files after rendering" do
       allow(message).to receive(:file_contents) { "foo" }
-      expect(message).to_not receive(:destroy)
+      expect(crypt).to_not receive(:destroy)
 
-      get :show, id: "1b0dad15-732e-45cc-8da4-749b4b21585d"
+      get :show, id: guid, key: key
     end
   end
 
   describe "downloading a file" do
+    let(:key)            { "test key" }
+    let(:guid)           { "1b0dad15-732e-45cc-8da4-749b4b21585d" }
     let(:message) do
       Message.new(
-        id: "1b0dad15-732e-45cc-8da4-749b4b21585d",
+        id: guid,
         file_contents: "c2VjcmV0IG1lc3NhZ2U=\n",
         file_extension: "txt",
       )
     end
+    let(:crypt)          { Crypt.new message }
 
     before do
-      allow(Message).to receive(:find).with("1b0dad15-732e-45cc-8da4-749b4b21585d") { message }
+      allow(Crypt).to receive(:find).with(guid, key) { crypt }
+      allow(crypt).to receive(:file_contents) { "secret message" }
     end
 
     it "sends a file" do
       expect(controller).to receive(:send_data).with("secret message", type: "text/plain", filename: "1b0dad15-732e-45cc-8da4-749b4b21585d.txt")
-      expect(message).to receive(:destroy)
+      expect(crypt).to receive(:destroy)
 
       # the view throws an error because we stub out send_file
-      expect{ get :download, id: "1b0dad15-732e-45cc-8da4-749b4b21585d"}.to raise_error ActionView::MissingTemplate 
+      expect{ get :download, id: guid, key: key}.to raise_error ActionView::MissingTemplate 
     end
   end
 end
--- spec/views/messages/show_spec.rb
+++ spec/views/messages/show_spec.rb
@@ -1,31 +1,18 @@
+require "crypt"
 require "rails_helper"
 
 describe "messages/show" do
-  let(:message) { Message.new(id: "1b0dad15-732e-45cc-8da4-749b4b21585d", text: "This is a test") }
-
-  before do
-    assign :message, message
-
-    render
-  end
-
-  it "displays the message" do
-    expect(rendered).to have_content "This is a test"
-  end
-
-  it "displays a notification about the message being deleted" do
-    expect(rendered).to have_content "This message has been deleted"
-  end
-
-  it "should not render a meta tag" do
-    expect(view.content_for(:head)).to eq nil
-  end
-
   context "when the message has a file" do
-    let(:message) { Message.new(id: "1b0dad15-732e-45cc-8da4-749b4b21585d", file_contents: "foo bar") }
+    let(:message) { Crypt.new Message.new(id: "1b0dad15-732e-45cc-8da4-749b4b21585d", file_contents: "foo bar", file_extension: "txt") }
+
+    before do
+      assign :message, message
+    end
 
     it "inserts the meta tag if the message has a file" do
-      expect(view.content_for(:head)).to include(download_url(message))
+      allow(message).to receive(:text) { "secret message" }
+      render
+      expect(view.content_for(:head)).to include(download_url(message.message, key: message.key))
     end
   end
 end
